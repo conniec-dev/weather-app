4a14968c5068e64feb75d9cb1b8795725bd06075
conniec-dev <conniecanelon97@gmail.com>
2022-03-30 11:43:49 -0500

    Interface changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { WEATHER_APP_ID } from "./constants.js";
 import moment from "moment-timezone";
 import './App.css';
 
-const MED_ID = 1699896
+const MED_ID = 3674962
 
 const api_url_five_days = `https://api.openweathermap.org/data/2.5/forecast?id=${MED_ID}&appid=1cda4e7ec4878b0bfaa93961d1294169&units=metric&mode=json`
 
@@ -38,51 +38,20 @@ const getLastElementOfList = (l) => {
   return l[l.length-1]
 }
 
-const addProp = (obj) => {
-  const { dt: timestamp } = obj
-  // console.log("addProp > timestamp: ", timestamp)
-  const timestampinMiliseconds = timestamp * 1000;
-  const timestampWithLocalTimezone = moment(timestampinMiliseconds).format("YYYY-MM-DD")
-  // console.log("timestampWithLocalTimezone", timestampWithLocalTimezone)
+const getDateWithLocalTimezone = (obj) => {
+  const { dt: timestampInSeconds } = obj
+  const timestamp = timestampInSeconds * 1000;
+  const dateWithLocalTimezone = moment(timestamp).format("YYYY-MM-DD")
   const newObj = {
     ...obj,
-    timestampWithLocalTimezone,
+    dateWithLocalTimezone,
   };
   return newObj
 }
 
 function App() {
   console.log("App rendering")
-  const [weatherData, setWeatherData] = useState({
-    day: "",
-    image: "",
-    tempMax: 0,
-    tempMin: 0
-  })
-  const [weatherData2, setWeatherData2] = useState({
-    day: "",
-    image: "",
-    tempMax: 0,
-    tempMin: 0
-  })
-  const [weatherData3, setWeatherData3] = useState({
-    day: "",
-    image: "",
-    tempMax: 0,
-    tempMin: 0
-  })
-  const [weatherData4, setWeatherData4] = useState({
-    day: "",
-    image: "",
-    tempMax: 0,
-    tempMin: 0
-  })
-  const [weatherData5, setWeatherData5] = useState({
-    day: "",
-    image: "",
-    tempMax: 0,
-    tempMin: 0
-  })
+  const [weatherData, setWeatherData] = useState([])
   // const [day, setDay] = useState(0);
   // const [image, setImage] = useState("");
   // const [tempMax, setTempMax] = useState(0);
@@ -95,64 +64,21 @@ function App() {
       //5-Day Forecast
       const response = await fetch(api_url_five_days)
       const data = await response.json()
-      console.log("data: ", data);
-
-      const newList = data.list.map(addProp)
-      console.log("new List: ", newList)
+      const newList = data.list.map(getDateWithLocalTimezone)
       
-      const groupedObject = groupBy(newList, "timestampWithLocalTimezone")
+      const groupedObject = groupBy(newList, "dateWithLocalTimezone")
       console.log("GROUPED OBJ: ", groupedObject)
       const newObject = valmap(groupedObject, getLastElementOfList)
-      console.log("New object: ", newObject)
       const dataList = Object.values(newObject)
-      
-      //Way to calculate time for 5 days
-      const dayOfWeekList = []
-      for (let element in dataList) {
-        const timestampInSecondsList = dataList[element].dt
-        const dateList = new Date(timestampInSecondsList * 1000);
-        dayOfWeekList.push(DAYS_OF_WEEK[dateList.getDay()])
-      }
-      console.log("dayOfWeekList: ", dayOfWeekList)
 
-      
+      const apiWeatherData = dataList.map(e => ({
+        day: DAYS_OF_WEEK[(new Date(e.dt * 1000)).getDay()],
+        image: e.weather[0].icon,
+        tempMax: Math.ceil(e.main.temp_max),
+        tempMin:Math.floor(e.main.temp_min)
+      }))
 
-      setWeatherData({
-        day: dayOfWeekList[0],
-        image: dataList[0].weather[0].icon,
-        tempMax: Math.ceil(dataList[0].main.temp_max),
-        tempMin:Math.floor(dataList[0].main.temp_min)
-      })
-      setWeatherData2({
-        day: dayOfWeekList[1],
-        image: dataList[1].weather[0].icon,
-        tempMax: Math.ceil(dataList[1].main.temp_max),
-        tempMin: Math.floor(dataList[1].main.temp_min)
-      })
-      setWeatherData3({
-        day: dayOfWeekList[2],
-        image: dataList[2].weather[0].icon,
-        tempMax: Math.ceil(dataList[2].main.temp_max),
-        tempMin:Math.floor(dataList[2].main.temp_min)
-      })
-      setWeatherData4({
-        day: dayOfWeekList[3],
-        image: dataList[3].weather[0].icon,
-        tempMax: Math.ceil(dataList[3].main.temp_max),
-        tempMin:Math.floor(dataList[3].main.temp_min)
-      })
-      setWeatherData5({
-        day: dayOfWeekList[4],
-        image: dataList[4].weather[0].icon,
-        tempMax: Math.ceil(dataList[4].main.temp_max),
-        tempMin:Math.floor(dataList[4].main.temp_min)
-      })
-      // setDay(dayOfWeek)
-
-      // setImage(data.weather[0].icon)
-      // setTempMax(Math.ceil(data.main.temp_max))
-      // setTempMin(Math.floor(data.main.temp_min))
-      
+      setWeatherData(apiWeatherData)
     }
     catch (error) {
       console.error(error)
@@ -163,34 +89,42 @@ function App() {
     getWeatherData()
   }, [getWeatherData])
 
+  const jsonData = require("./cities_list.json")
+
+
   return (
     <div className="App">
+      <div className="city__selector">
+        <div className="city__selector__container">
+        <label for="cities" className="label__text">Choose a city: </label>
+          <select name="cities" id="cities" className="selector">
+            {jsonData.map((e, i) => (
+              <option value="city">{e.name}</option>
+            ))}
+          </select>
+          <br></br>
+          <div className="button">
+	          <div className="container">
+              send
+	          </div>
+          </div>
+        </div>
+      </div>
+      <div className="city__name">
+        <div className="city__name__container">
+          Medellin
+        </div>
+      </div>
       <div className="dayforecast__container">
-        <DayForecast 
-        day={weatherData.day}
-        image={`http://openweathermap.org/img/wn/${weatherData.image}@2x.png`}
-        tempMax={weatherData.tempMax}
-        tempMin={weatherData.tempMin} />
-        <DayForecast 
-        day={weatherData2.day}
-        image={`http://openweathermap.org/img/wn/${weatherData2.image}@2x.png`}
-        tempMax={weatherData2.tempMax}
-        tempMin={weatherData2.tempMin} />
-        <DayForecast 
-        day={weatherData3.day}
-        image={`http://openweathermap.org/img/wn/${weatherData3.image}@2x.png`}
-        tempMax={weatherData3.tempMax}
-        tempMin={weatherData3.tempMin} />
-        <DayForecast 
-        day={weatherData4.day}
-        image={`http://openweathermap.org/img/wn/${weatherData4.image}@2x.png`}
-        tempMax={weatherData4.tempMax}
-        tempMin={weatherData4.tempMin} />
-        <DayForecast 
-        day={weatherData5.day}
-        image={`http://openweathermap.org/img/wn/${weatherData5.image}@2x.png`}
-        tempMax={weatherData5.tempMax}
-        tempMin={weatherData5.tempMin} />
+        {weatherData.map((e, i) => (
+          <DayForecast
+            key={i}
+            day={e.day}
+            image={`http://openweathermap.org/img/wn/${e.image}@2x.png`}
+            tempMax={e.tempMax}
+            tempMin={e.tempMin}
+          />
+        ))}
       </div>
     </div>
   );
